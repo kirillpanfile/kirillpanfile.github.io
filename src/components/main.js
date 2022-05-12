6b972073ae108f4cd0fb1caf69d0eb91395b721c
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-05-12 09:02:59 +0300

    adding dist

--- src/components/main.js
+++
@@ -1,8 +0,0 @@
-import appBubbles from "./appBubbles";
-import appMenu from "./appMenu";
-import appButton from "./appButton";
-import appLogo from "./appLogo";
-import appCanvas from "./appCanvas";
-import appLoader from "./appLoader";
-
-export default [appBubbles, appMenu, appButton, appLogo, appCanvas, appLoader];
